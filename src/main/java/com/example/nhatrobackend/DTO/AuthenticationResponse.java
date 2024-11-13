e80a9530f243a3cecbbe71db149a835104314cb5
Van Tin <vantin22112003@gmail.com>
2024-11-13 22:56:25 +0700

    Hoan thanh chuc nang dang nhap

---
+++ src/main/java/com/example/nhatrobackend/DTO/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.nhatrobackend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    boolean authenticated;
+}
---
+++ src/main/java/com/example/nhatrobackend/Entity/Field/Role.java
@@ -0,0 +1,8 @@
+package com.example.nhatrobackend.Entity.Field;
+
+public enum Role {
+    TENANT,
+    LANDLORD,
+    MODERATOR,
+    ADMIN;
+}
