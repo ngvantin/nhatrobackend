5eb0faaae420db17ace2fd6d8e0f5ec3e16d90ca
Van Tin <vantin22112003@gmail.com>
2024-11-27 15:49:27 +0700

    Repository tinh huyen xa

---
+++ src/main/java/com/example/nhatrobackend/Responsitory/Administrative/DistrictRepository.java
@@ -0,0 +1,6 @@
+package com.example.nhatrobackend.Responsitory.Administrative;
+
+import com.example.nhatrobackend.Entity.Administrative.District;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistrictRepository extends JpaRepository<District, String> {}
---
+++ src/main/java/com/example/nhatrobackend/Responsitory/Administrative/WardRepository.java
@@ -0,0 +1,6 @@
+package com.example.nhatrobackend.Responsitory.Administrative;
+
+import com.example.nhatrobackend.Entity.Administrative.Ward;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WardRepository extends JpaRepository<Ward, String> {}
