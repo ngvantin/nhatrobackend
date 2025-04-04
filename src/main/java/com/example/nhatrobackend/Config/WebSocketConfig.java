d46ebc9cd6473ea90b2194d63113d47089396b78
Van Tin <vantin22112003@gmail.com>
2025-04-04 09:40:39 +0700

    cấu hình web socket

---
+++ src/main/java/com/example/nhatrobackend/Config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.example.nhatrobackend.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Cho phép React kết nối với WebSocket của Spring Boot
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+
+//        registry.addEndpoint("/chat");
+//        registry.addEndpoint("/chat").withSockJS();
+    }
+}
+
