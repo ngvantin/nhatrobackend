0317237c8fd43e61ffdf0fafb49315549f1de583
Van Tin <vantin22112003@gmail.com>
2025-06-02 23:44:14 +0700

    thêm phần giao dịch trang admin

--- src/main/java/com/example/nhatrobackend/Service/PaymentHistoryService.java
+++ src/main/java/com/example/nhatrobackend/Service/PaymentHistoryService.java
@@ -1,6 +1,8 @@
 package com.example.nhatrobackend.Service;
 
 import com.example.nhatrobackend.DTO.response.PaymentHistoryResponse;
+import com.example.nhatrobackend.DTO.response.AdminPaymentHistoryResponse;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.List;
 public interface PaymentHistoryService {
     List<PaymentHistoryResponse> getPaymentHistoriesByUserId(Integer userId);
     List<Long> getMonthlyRevenue(LocalDateTime startDate, LocalDateTime endDate);
+    PaymentHistoryResponse getPaymentDetailsById(Integer userId, Long paymentId);
+    Page<PaymentHistoryResponse> getAllPaymentHistories(int page, int size, String sortBy, String direction);
+    AdminPaymentHistoryResponse getAdminPaymentDetailsById(Long paymentId);
 }
